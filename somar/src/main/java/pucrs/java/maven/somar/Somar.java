14c4abac08cff77f0f5138abeaa2ed8fb4bcd912
Marco Mangan <marco.mangan@gmail.com>
2019-06-06 14:29:46 -0300

    #5

--- somar/src/main/java/pucrs/java/maven/somar/Somar.java
+++ somar/src/main/java/pucrs/java/maven/somar/Somar.java
@@ -4,25 +4,38 @@ package pucrs.java.maven.somar;
  * 
  * @author Marco Mangan
  */
-public class Somar 
-{
+public class Somar {
 	/**
 	 * 
 	 * @param args
 	 */
-    public static void main( String[] args )
-    {
-    	int a = Integer.parseInt(args[0]);
-    	int b = Integer.parseInt(args[1]);
-        System.out.println( somar(a, b) );
-    }
+	public static void main(String[] args) {
+		try {
+			verificarNumeroDeParametros(args);
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+			System.out.println(somar(a, b));
+		} catch (NumberFormatException e) {
+			System.err.println("Valor inválido!");
+			return;
+		} catch (IllegalArgumentException e) {
+			System.err.println("Número incorreto de argumentos!");
+			return;
+		}
+	}
 
-    /**
-     * 
-     * @param a
-     * @param b
-     * @return
-     */
+	static void verificarNumeroDeParametros(String[] args) {
+		if (args.length != 2) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	static int somar(int a, int b) {
 		return a + b;
 	}
